ef710ddcd81d1f9ce01996a7e281ec73aaeb4a4b
Fritak <info@fritak.eu>
2019-12-10 15:47:46 +0100

    Updates

---
+++ precache-manifest.9e87ae1230f9060a350db694ee115567.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388d73964b66b98d8c3a85183f3c9ac6",
+    "url": "/when-gm/index.html"
+  },
+  {
+    "revision": "fa579737cf260a2afc4c",
+    "url": "/when-gm/static/js/2.641ea648.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/when-gm/static/js/2.641ea648.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d029ed7a764c6d4d9aa8",
+    "url": "/when-gm/static/js/main.9c4d018b.chunk.js"
+  },
+  {
+    "revision": "7beca856923e4765ebee",
+    "url": "/when-gm/static/js/runtime-main.ac4b44e3.js"
+  }
+]);
